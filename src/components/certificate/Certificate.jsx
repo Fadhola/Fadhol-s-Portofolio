520b918b13405b3417dd2c9916420dbc70125a6e
Fadhola <fadholasandi@gmail.com>
2025-07-18 17:59:49 +0700

    update certi

--- src/components/certificate/Certificate.jsx
+++ src/components/certificate/Certificate.jsx
@@ -24,7 +24,7 @@ const certificates = [
   },
   {
     id: 4,
-    title: "Certificate 3",
+    title: "Bootcamp Hacktiv8 Full-day Data Science",
     image: "/certificates/certiHacktiv8.png",
   },
 ];
